10da0b8ebe1f7adbd7791e622b3f92ba5dffc06d
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-02-19 11:15:43 -0500

    linking routeds work

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -9,40 +9,46 @@ const Navbar = () => {
         // <Router>
             <div>
                 <nav className="header__navigator">
-                    {/* <div className="header"> */}
 
-
-
-                    <p className="header__logo"><Link to="/">
+                    <p className="header__logo"> 
+                    <Link className="link" to="/">
                     {" "}
                     CRYPTO COLLECTION. <br />
                     LINK
-
-                    </Link></p>
+                    </Link>
+                    </p>
 
 
                     <div className="registration__buttons">
                     <Login />
                     <Signup />
                     </div>
+
                 </nav>
+
+<div>
 
                 <nav className="router__tabs">
-
-                    <div className="my__collection__tab"><Link to="/collection">
+                    <Link className="link" to="/collection">
+                    <div className="my__collection__tab">
                         <span className="tab__text"> My Collection</span>
-                    </Link></div>
+                    </div>
+                    </Link>
 
-                    <div className="my__watchlist__tab"><Link to="/watchlist">
+                    <Link className="link" to="/watchlist">
+                    <div className="my__watchlist__tab">
                         <span className="tab__text">My Watchlist</span>
-                    </Link></div>
+                    </div>
+                    </Link>
 
                     
-                    <div className="shop__tab"><Link to="/shop">
-
-                    <span className="tab__text">Get T-Shirt</span>
-                    </Link></div>
+                        <Link className="link" to="/shop">
+                    <div className="shop__tab">
+                        <span className="tab__text">Get T-Shirt</span>
+                    </div>
+                    </Link>
                 </nav>
+</div>
             </div>
         /* </Router> */
   );
